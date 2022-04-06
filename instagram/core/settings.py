2f5f3f7f045656dd5bb9ed4c3f31e1632c8f7d1d
beka <beka1990.30.10@gmail.com>
2022-04-06 14:58:15 +0600

    Добавил приложение api_v1, а так же обновил зависимости

--- instagram/core/settings.py
+++ instagram/core/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'webapp',
     'accounts',
-    'phonenumber_field'
+    'phonenumber_field',
+    'api_v1'
 ]
 
 MIDDLEWARE = [
